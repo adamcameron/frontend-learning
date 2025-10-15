18bb36d9b13a2ade8911e0e5bd21e5d5c39896f3
Adam Cameron <cameron.adam+github@gmail.com>
2025-10-15 17:37:26 +0100

    FL-38: self code review

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         fl38: resolve(__dirname, 'pages/fl-38/index.html'),
-      }
-    }
+      },
+    },
+  },
 })
