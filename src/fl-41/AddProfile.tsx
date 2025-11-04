894ab4b5ce60ead18f1f9a2b295c866800a0d441
Adam Cameron <cameron.adam+github@gmail.com>
2025-11-04 13:20:25 +0000

    FL-40: didn't mean to commit this stuff

--- src/fl-41/AddProfile.tsx
+++
@@ -1,7 +0,0 @@
-export default function AddProfile() {
-  return (
-    <section data-testid="add-profile">
-      <h1>hi</h1>
-    </section>
-  )
-}
--- src/fl-41/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AddProfile from './AddProfile.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AddProfile />
-  </StrictMode>
-)
